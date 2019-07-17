92ee0a1bd6dbfc30b6e11de6e5fc076085bd6e5a
Al Miller <al.miller@ajmiller.net>
2019-07-17 13:03:38 -0700

    add lambda

--- services/time/time.js
+++ services/time/time.js
@@ -1,4 +1,4 @@
-var time = require('services/backend/time');
+var time = require('services/time/time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
     currentTime.setTimezone("America/Los_Angeles");
