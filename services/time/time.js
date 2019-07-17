aaaaa54745a71f209e3aa99ec1614ee7c024234b
Al Miller <al.miller@ajmiller.net>
2019-07-17 15:29:59 -0700

    update lambda

--- services/time/time.js
+++ services/time/time.js
@@ -2,6 +2,6 @@ exports.handler = (event, context, callback) => {
     var currentTime = new Date();
     callback(null, {
         statusCode: '200',
-        body: 'The time somewhere is: ' + currentTime.toString(),
+        body: 'The time is: ' + currentTime.toString(),
     });
 };
