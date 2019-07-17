e37c3216c1e910cfa6b90ad124ba4ecb39a2c537
Al Miller <al.miller@ajmiller.net>
2019-07-17 15:02:36 -0700

    fix lambda

--- services/time/time.js
+++ services/time/time.js
@@ -1,6 +1,5 @@
 exports.handler = (event, context, callback) => {
     var currentTime = new Date();
-    currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
         body: 'The time in Seattle is: ' + currentTime.toString(),
