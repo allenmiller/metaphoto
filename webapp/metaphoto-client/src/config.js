8ec25d798b75e305d23be34e65d8c7c12f64485b
Al Miller <al.miller@ajmiller.net>
2019-07-19 16:16:19 -0700

    at the end of https://serverless-stack.com/chapters/configure-aws-amplify.html

---
+++ webapp/metaphoto-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-west-2",
+        BUCKET: "metaphoto-us-west-2-dev-metaphotodatabucket-8jec596nfltl"
+    },
+    apiGateway: {
+        REGION: "us-west-2",
+        URL: "https://api.metaphoto.ajmiller.net/dev"
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_1Xx2iVgku",
+        APP_CLIENT_ID: "6tiq5dic0b9dk6msnj07j9me3t",
+        IDENTITY_POOL_ID: "us-west-2:857b1490-f566-4039-9f20-5036fa7e5f71"
+    }
+};
