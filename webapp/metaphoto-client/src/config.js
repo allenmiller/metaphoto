1410bb79983fee47dfe699b4dac0123476898213
Al Miller <al.miller@ajmiller.net>
2019-07-24 15:42:45 -0700

    REACT_ENV -> REACT_APP

--- webapp/metaphoto-client/src/config.js
+++ webapp/metaphoto-client/src/config.js
@@ -9,8 +9,8 @@ export default {
     },
     cognito: {
         REGION: "us-west-2",
-        USER_POOL_ID: process.env.REACT_ENV_USER_POOL_ID,
-        APP_CLIENT_ID: process.env.REACT_ENV_USER_POOL_CLIENT_ID,
-        IDENTITY_POOL_ID: process.env.REACT_ENV_IDENTITY_POOL_ID
+        USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
+        APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+        IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID
     }
 };
