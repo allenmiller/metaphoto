4e52bbef09cd4ab9dfed2ca9502221001519dc95
Al Miller <al.miller@ajmiller.net>
2019-07-25 06:57:30 -0700

    configure CORS

--- services/time/time.js
+++ services/time/time.js
@@ -1,7 +1,12 @@
 exports.handler = (event, context, callback) => {
-    var currentTime = new Date();
-    callback(null, {
+    let currentTime = new Date();
+    let response = {
         statusCode: '200',
         body: 'The time is: ' + currentTime.toString(),
-    });
+        headers: {
+            'Access-Control-Allow-Origin': 'https://metaphoto.ajmiller.net',
+            'Access-Control-Allow-Credentials': true,
+        }
+    };
+    callback(null, response);
 };
