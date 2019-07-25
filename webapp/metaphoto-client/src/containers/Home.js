c1a040920c57b05ace8294c270997d14fb710e1d
Al Miller <al.miller@ajmiller.net>
2019-07-24 21:38:06 -0700

    experiment with amplify API.get()

--- webapp/metaphoto-client/src/containers/Home.js
+++ webapp/metaphoto-client/src/containers/Home.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import "./Home.css";
 
+import { API } from "aws-amplify";
+
 export default class Home extends Component {
+
+    getTime = async () => {
+        let apiName = "";   // TODO: define actual API
+        let apiPath = "time";
+        let myInit = {};
+        let time =  await API.get(apiName, apiPath, myInit);
+        this.setState({time: time});
+    };
+
     render() {
+
         return (
             <div className="Home">
                 <div className="lander">
+                    getTime();
                     <h1>Metaphoto</h1>
                     <p>Keep track of all that photographic metadata</p>
+                    <p>{this.state.time}</p>
                 </div>
             </div>
         );
