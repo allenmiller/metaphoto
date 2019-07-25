58c27c1756c5fe9a6d5a2f2f4600c5a9267b3b28
Al Miller <al.miller@ajmiller.net>
2019-07-25 07:11:00 -0700

    return content as JSON

--- services/time/time.js
+++ services/time/time.js
@@ -2,10 +2,11 @@ exports.handler = (event, context, callback) => {
     let currentTime = new Date();
     let response = {
         statusCode: '200',
-        body: 'The time is: ' + currentTime.toString(),
+        body: '{data: "The time is: ' + currentTime.toString() + '"',
         headers: {
             'Access-Control-Allow-Origin': 'https://metaphoto.ajmiller.net',
             'Access-Control-Allow-Credentials': true,
+            'Content-Type': 'application/json'
         }
     };
     callback(null, response);
